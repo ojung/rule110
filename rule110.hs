7e851be0cc18e6b755b8796b4163e3fc236dffe7
Oskar Jung <oskar@mbr-targeting.com>
2015-06-15 13:21:38 +1200

    Add type annotations

--- rule110.hs
+++ rule110.hs
@@ -3,19 +3,24 @@ module Main where
 import Data.List
 
 blank = ' '
-symbol = '~'
+symbol = '@'
 width = 270
 
+xor :: Char -> Char -> Bool
 a `xor` b = not (isAlive a && isAlive b) && (isAlive a || isAlive b)
 
+notBut :: Char -> Char -> Bool
 a `notBut` b = not (isAlive a) && isAlive b
 
+isAlive :: Char -> Bool
 isAlive a = a == symbol
 
+main :: IO ()
 main =
   let first = [if (x == width) then symbol else blank | x <- [1 .. width]]
   in doRule110 first
 
+doRule110 :: [Char] -> IO ()
 doRule110 list
   | first == symbol = do
     print list
@@ -25,11 +30,14 @@ doRule110 list
   where first:_ = list
 
 
+getTransposedList :: [Char] -> [[Char]]
 getTransposedList list =
   transpose [(blank:list), list, ((tail list) ++ [blank])]
 
+calculateNext :: [Char] -> IO ()
 calculateNext list =
   let decoratedList = getTransposedList list
   in doRule110 [getCell pre x suc | [pre, x, suc] <- decoratedList]
 
+getCell :: Char -> Char -> Char -> Char
 getCell pre x suc = if (pre `notBut` x || x `xor` suc) then symbol else blank
