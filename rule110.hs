519b57f005bd27a04b2ae6422e5838efdac3c0d9
Oskar Jung <oskar@mbr-targeting.com>
2015-06-15 12:55:14 +1200

    Terminate if first cell is alive

--- rule110.hs
+++ rule110.hs
@@ -4,7 +4,7 @@ import Data.List
 
 blank = ' '
 symbol = '~'
-width = 110
+width = 270
 
 a `xor` b = not (isAlive a && isAlive b) && (isAlive a || isAlive b)
 
@@ -16,13 +16,20 @@ main =
   let first = [if (x == width) then symbol else blank | x <- [1 .. width]]
   in doRule110 first
 
-doRule110 list = do
-  print list
-  calculateNext listWithPreAndSuc
-  where listWithPreAndSuc = [x | x <- getTransposedList list]
+doRule110 list
+  | first == symbol = do
+    print list
+  | otherwise = do 
+    print list
+    calculateNext list
+  where first:_ = list
+
 
 getTransposedList list =
   transpose [(blank:list), list, ((tail list) ++ [blank])]
 
-calculateNext decoratedList =
-  doRule110 [if ( pre `notBut` x || x `xor` suc) then symbol else blank | [pre, x, suc] <- decoratedList]
+calculateNext list =
+  let decoratedList = getTransposedList list
+  in doRule110 [getCell pre x suc | [pre, x, suc] <- decoratedList]
+
+getCell pre x suc = if (pre `notBut` x || x `xor` suc) then symbol else blank
