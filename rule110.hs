8651a652dd4e305e32cf1a2b62c99612194e7c84
Oskar Jung <oskar@mbr-targeting.com>
2015-06-16 20:19:12 +1200

    Make first generation random and number of iterations configurable

--- rule110.hs
+++ rule110.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import Data.List
+import System.Random
 
 blank = ' '
 symbol = '@'
-width = 270
+width = 140
+iterations = 50
 
 xor :: Char -> Char -> Bool
 a `xor` b = not (isAlive a && isAlive b) && (isAlive a || isAlive b)
@@ -17,27 +19,29 @@ isAlive a = a == symbol
 
 main :: IO ()
 main =
-  let first = [if (x == width) then symbol else blank | x <- [1 .. width]]
-  in doRule110 first
+  let first = [if x then symbol else blank | x <- getRandomBools]
+  in doRule110 first 0
 
-doRule110 :: [Char] -> IO ()
-doRule110 list
-  | first == symbol = do
-    print list
-  | otherwise = do 
-    print list
-    calculateNext list
-  where first:_ = list
+getRandomBools :: [Bool]
+getRandomBools = take (width + 2) $ randoms (mkStdGen 1) :: [Bool]
 
+doRule110 :: [Char] -> Int -> IO ()
+doRule110 list@(_:xs) count
+  | count == iterations = do
+    print (init xs)
+  | otherwise = do
+    print (init xs)
+    calculateNext list count
 
 getTransposedList :: [Char] -> [[Char]]
 getTransposedList list =
   transpose [(blank:list), list, ((tail list) ++ [blank])]
 
-calculateNext :: [Char] -> IO ()
-calculateNext list =
+calculateNext :: [Char] -> Int -> IO ()
+calculateNext list count =
   let decoratedList = getTransposedList list
-  in doRule110 [getCell pre x suc | [pre, x, suc] <- decoratedList]
+  in doRule110 [getCell pre x suc | [pre, x, suc] <- decoratedList] (count + 1)
 
 getCell :: Char -> Char -> Char -> Char
 getCell pre x suc = if (pre `notBut` x || x `xor` suc) then symbol else blank
+
