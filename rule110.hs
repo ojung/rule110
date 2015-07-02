b778625473960e3b5c66106a825efb9c53798a60
Oskar Jung <oskar@mbr-targeting.com>
2015-07-03 09:59:41 +1200

    Refactor code so it compiles with -Wall -Werror

--- rule110.hs
+++ rule110.hs
@@ -3,31 +3,38 @@ module Main where
 import Data.List
 import System.Random
 
+blank :: Char
 blank = ' '
+
+symbol :: Char
 symbol = '@'
+
+width :: Int
 width = 140
-iterations = 50
+
+iterations :: Integer
+iterations = 500
 
 main :: IO ()
 main = do
   gen <- newStdGen
-  let randoms = take (width + 2) $ randomRs (True, False) gen :: [Bool]
-      first = [if x then symbol else blank | x <- randoms]
+  let random_bools = take (width + 2) $ randomRs (True, False) gen :: [Bool]
+      first = [if x then symbol else blank | x <- random_bools]
   prettyPrint $ doRule110 first [] 0
 
 prettyPrint :: [String] -> IO ()
+prettyPrint [] = return ()
 prettyPrint [x] = putStrLn x
 prettyPrint (x:xs) = do
   putStrLn x
   prettyPrint xs
 
 doRule110 :: String -> [String] -> Integer -> [String]
-doRule110 prev@(_:xs) acc count
+doRule110 prev acc count
   | count == iterations = acc
-  | otherwise =
-    let next = [getCell pre x suc | [pre, x, suc] <- decoratedList]
-    in doRule110 next (acc ++ [next]) (count + 1)
-  where decoratedList = transpose [blank:prev, prev, tail prev ++ [blank]]
+  | otherwise = doRule110 next_gen (acc ++ [next_gen]) (count + 1)
+    where decorated_list = transpose [blank:prev, prev, tail prev ++ [blank]]
+          next_gen = [getCell pre x suc | [pre, x, suc] <- decorated_list]
 
 getCell :: Char -> Char -> Char -> Char
 getCell pre x suc = if pre `notBut` x || x `xor` suc then symbol else blank
