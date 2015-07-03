34d1b1050e2ddf679be0338c3f6e2b20a8542018
Oskar Jung <oskar@mbr-targeting.com>
2015-07-03 12:23:51 +1200

    Use Bools instead of Chars until printing
    Performance gains
    Before:
    ```
    > time ./rule110 140 1000 > /dev/null
    0.05s user 0.00s system 82% cpu 0.063 total
    > time ./rule110 140 10000 > /dev/null
    2.59s user 0.02s system 99% cpu 2.630 total
    > time ./rule110 140 100000 > /dev/null
    907.03s user 6.93s system 98% cpu 15:26.70 total
    ```
    
    after
    ```
    > time ./rule110 140 1000 > /dev/null
    0.04s user 0.00s system 95% cpu 0.043 total
    > time ./rule110 140 10000 > /dev/null
    2.43s user 0.02s system 99% cpu 2.455 total
    > time ./rule110 140 100000 > /dev/null
    885.87s user 5.16s system 99% cpu 14:56.31 total
    ```

--- rule110.hs
+++ rule110.hs
@@ -18,36 +18,26 @@ main = do
     [width, iterations] -> do
       gen <- newStdGen
       let w = read width + 2
-          bools = take w $ randomRs (True, False) gen
-          first = [if x then symbol else blank | x <- bools]
+          first = take w $ randomRs (True, False) gen
       prettyPrint $ doRule110 first [] $ read iterations
     _ -> do
       putStrLn "Error! Usage: ./rule110 <width> <iterations>"
       exitFailure
 
-prettyPrint :: [String] -> IO ()
-prettyPrint [] = return ()
-prettyPrint [x] = putStrLn x
-prettyPrint (x:xs) = do
-  putStrLn x
-  prettyPrint xs
+prettyPrint :: [[Bool]] -> IO ()
+prettyPrint nestedList =
+  mapM_ putStrLn generations
+  where generations = [transform list | list <- nestedList]
+        transform list = [if x then symbol else blank | x <- list]
 
-doRule110 :: String -> [String] -> Integer -> [String]
+
+doRule110 :: [Bool] -> [[Bool]] -> Integer -> [[Bool]]
 doRule110 prev acc count
   | count == 0 = acc
   | otherwise = doRule110 next_gen (acc ++ [next_gen]) (count - 1)
-    where decorated_list = transpose [blank:prev, prev, tail prev ++ [blank]]
+    where decorated_list = transpose [False:prev, prev, tail prev ++ [False]]
           next_gen = [getCell pre x suc | [pre, x, suc] <- decorated_list]
 
-getCell :: Char -> Char -> Char -> Char
-getCell pre x suc = if pre `notBut` x || x `xor` suc then symbol else blank
-
-xor :: Char -> Char -> Bool
-a `xor` b = not (isAlive a && isAlive b) && (isAlive a || isAlive b)
-
-notBut :: Char -> Char -> Bool
-a `notBut` b = not (isAlive a) && isAlive b
-
-isAlive :: Char -> Bool
-isAlive a = a == symbol
+getCell :: Bool -> Bool -> Bool -> Bool
+getCell pre x suc = not pre && x || not (x && suc) && (x || suc)
 
